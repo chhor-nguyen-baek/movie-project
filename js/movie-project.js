98ca4d6f4259f2a0814764874122f4bbe373d55f
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-03-21 12:25:43 -0500

    Ready for lunch break - switch driver to chhor after

--- js/movie-project.js
+++ js/movie-project.js
@@ -1,4 +1,6 @@
-"use strict"
+
+(function () {
+    "use strict"
 // const reviewObj = {
 //     restaurant_id: 1,
 //     name: 'Codey',
@@ -19,4 +21,26 @@ fetch('https://glitter-furtive-transport.glitch.me/movies')
     .catch( error => console.error(error) ) /* handle errors */
     .then(function (data){
         console.log(data);
-    })
+        console.log(data[0].title)
+
+        var addHTML = "";
+        for(let i = 0; i < 6; i++){
+            addHTML += "<div class=''>"
+            addHTML +=  "<h5>Title: " + data[i].title + "</h5><br>"
+            addHTML +=  "<h5>Director: " + data[i].director + "</h5><br>"
+            addHTML +=  "<h5>Rating: " + data[i].rating + "</h5><br>"
+            addHTML +=  "<h5>Genre: " + data[i].genre + "</h5><br>"
+            addHTML +=  "</div>"
+            addHTML += "<hr>"
+        }
+
+        document.getElementById('test-push').innerHTML=addHTML;
+
+
+
+
+
+
+    })//end of bracket
+
+})();
