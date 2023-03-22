5f527480407439e015dec9fae1dae53f31be9dc0
Anthony Chhor <anthonychhor@Anthonys-MacBook-Pro.local>
2023-03-22 13:43:22 -0500

    updated to adding a movie

--- js/movie-project.js
+++ js/movie-project.js
@@ -2,17 +2,17 @@
     "use strict"
 
     // FUNCTION FOR PAGE LOAD
-    window.onload = function() {
+    window.onload = function () {
         var loadingScreen = document.getElementById('loading-screen');
         var loadingCircle = document.getElementById('loader');
         var htmlContent = document.getElementById('content');
         var loadContainer = document.getElementById('loader-container');
 
-        setTimeout(function() {
-        loadContainer.style.display = 'none';
-        loadingCircle.style.display = 'none';
-        loadingScreen.style.display = 'none';
-        htmlContent.style.display = 'block';
+        setTimeout(function () {
+            loadContainer.style.display = 'none';
+            loadingCircle.style.display = 'none';
+            loadingScreen.style.display = 'none';
+            htmlContent.style.display = 'block';
         }, 3000)
     }
 
@@ -46,12 +46,14 @@
             })
             .catch(error => console.error(error)) /* handle errors */
     }
+
     movieInputForm();
 
 
     // FUNCTION TO ADD A MOVIE
 
-    function addMovie() {
+    function addMovie(e) {
+        e.preventDefault();
 
         var newTitle = document.querySelector('#title')
         var newDirector = document.querySelector('#director')
@@ -64,41 +66,30 @@
             genre: newGenre.value
         };
         const url = 'https://glitter-furtive-transport.glitch.me/movies';
-        const options = {
+        fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(newMovie),
-        };
-        fetch(url, options)
+        })
+
+        fetch(url)
             .then(response => response.json())
-            .then(function (newMovie) {
-                var addHTML = "";
-                for (let i = 0; i < newMovie.length; i++) {
-                    addHTML += "<div class='w-25 card col-6 my-3 mx-4'>"
-                    addHTML += "<h5>Title: " + newMovie[i].title + "</h5><br>"
-                    addHTML += "<h5>Director: " + newMovie[i].director + "</h5><br>"
-                    addHTML += "<h5>Rating: " + newMovie[i].rating + "</h5><br>"
-                    addHTML += "<h5>Genre: " + newMovie[i].genre + "</h5><br>"
-                    addHTML += "</div>"
-                    addHTML += "<hr>"
-                    $('#test-push').append(addHTML);
-                }
-            })
-        console.log(addMovie);
+            .then(() => movieInputForm()); {
+        }
     }
+
     var addMovieBtn = document.getElementById("new-movie-btn");
     addMovieBtn.addEventListener("click", addMovie);
 
     // FUNCTION TO DELETE MOVIE
 
-    function deleteMovie(){
+    function deleteMovie() {
         console.log("deleting...........");
         var selectedID = document.querySelector('#deleteMovie')
-        const reviewObj = {
-        };
-        const url = 'https://glitter-furtive-transport.glitch.me/movies/'+selectedID.value;
+        const reviewObj = {};
+        const url = 'https://glitter-furtive-transport.glitch.me/movies/' + selectedID.value;
         const options = {
             method: 'DELETE',
             headers: {
@@ -107,15 +98,15 @@
             body: JSON.stringify(reviewObj),
         };
         fetch(url, options)
-            .then( response => console.log(response) ) /* review was created successfully */
-            .then (function (deleteMovie){
+            .then(response => console.log(response)) /* review was created successfully */
+            .then(function (deleteMovie) {
                 console.log(deleteMovie);
             })
-            .catch( error => console.error(error) ); /* handle errors */
+            .catch(error => console.error(error)); /* handle errors */
     }
+
     var deleteBtn = document.querySelector("#removeMovie");
     deleteBtn.addEventListener("click", deleteMovie);
-
 
 
 })();
